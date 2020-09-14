16796f6b4fc5e7fbe367c87b935408f010805ca6
hideaki999 <61974678+hideaki999@users.noreply.github.com>
2020-09-15 07:20:46 +0900

    jobsコントローラー new create edit update destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,34 @@
 class JobsController < ApplicationController
+
   def index
   end
 
   def new
+    @job = Job.new
   end
 
   def create
+    Job.create(job_params)
   end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    job = Job.find(params[:id])
+    if job.user_id == current_user.id 
+      job.update(job_params)
+  end
+
+  def destroy
+    job = Job.find(params[:id])
+    job.destroy if job.user_id == current_user.id
+  end
+
+  private
+  def job_params 
+    params.require(:job).permit(:job_text)
+  end
+
 end
